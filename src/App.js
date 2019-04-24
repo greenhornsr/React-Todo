fe9c38c697567407ab36b887da07d34bb264bad3
greenhornsr <srollins11279@gmail.com>
2019-04-24 16:18:18 -0700

    adjusted array

--- src/App.js
+++ src/App.js
@@ -2,24 +2,25 @@ import React from 'react';
 import TodoForm from './components/TodoComponents/TodoForm'
 import TodoList from './components/TodoComponents/TodoList';
 
-const initialTodo = [
-  {
-    task: 'Organize Garage',
-    id: 1528817077286,
-    completed: false
-  },
-  {
-    task: 'Bake Cookies',
-    id: 1528817084358,
-    completed: false
-  }
-];
+// const initialTodo = [
+//   {
+//     task: 'Organize Garage',
+//     id: 1528817077286,
+//     completed: false
+//   },
+//   {
+//     task: 'Bake Cookies',
+//     id: 1528817084358,
+//     completed: false
+//   }
+// ];
 
 class App extends React.Component {
     constructor(){
       super()
       this.state = {
-        todoList: initialTodo,
+        // todoList: initialTodo,
+        todoList: [],
         todoElements: {
           task: '',
           id: '',
