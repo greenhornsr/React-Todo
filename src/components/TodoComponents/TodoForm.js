bd1d4b7e7ea7f63355ca94101d52512609e5cdad
greenhornsr <srollins11279@gmail.com>
2019-04-25 16:02:51 -0700

    updated styles

--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -2,11 +2,11 @@ import React from 'react'
 
 const TodoForm = (props) => {
     return (
-        <section>
+        <section className="form-section">
             <form onSubmit={props.addTask}>
-                <input placeholder='Add Task Here' value={props.todoForm.task} onChange={props.handleChanges} name="task"></input>
-                <button type="submit">Add Todo</button>
-                <button type="button" onClick={props.removeSelected}>Clear Completed</button>
+                <input className="input-1" placeholder='Add WORK Here' value={props.todoForm.task} onChange={props.handleChanges} name="task"></input>
+                <button className="buttons b-1" type="submit">Make More Work</button>
+                <button className="buttons b-2"type="button" onClick={props.removeSelected}>Clear Completed</button>
             </form>
         </section>
     )
